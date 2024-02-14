a90f3fedeba27214674928328dd46dc4f77dceec
banyanshade-software <braun.daniel@free.fr>
2024-02-14 18:04:54 +0100

    serial.c compile

--- SimpleBle/serial/serial.c
+++ SimpleBle/serial/serial.c
@@ -20,28 +20,44 @@ extern UART_HandleTypeDef huart4;
 extern DMA_HandleTypeDef hdma_lpuart1_tx;
 extern DMA_HandleTypeDef hdma_uart4_tx;
 
+extern osThreadId comTaskHandle;
+extern osThreadId bleTaskHandle;
+
 typedef struct {
+	osThreadId          taskHandle;
 	UART_HandleTypeDef *uart;
 	DMA_HandleTypeDef  *txdma;
 	uint8_t            *rxbuf;
+	uint8_t            *txbuf;
 	uint16_t            rxbuflen;
-	uint16_t            rxidx;
-	uint8_t             txonprogress;
+	uint16_t            txbuflen;
+	volatile uint16_t   rxidx;
+	volatile uint8_t    txonprogress;
 } serial_t;
 
 static uint8_t rxbuf0[256];
 static uint8_t rxbuf1[256];
 
+#define COMMON_INIT .taskHandle=NULL, .rxidx=0, .txonprogress=0
+
 
 #define NUM_SERIALS 2
 serial_t serials[NUM_SERIALS] = {
-		{ .rxidx=0, .uart=&hlpuart1, .txdma=&hdma_lpuart1_tx, .rxbuf=rxbuf0, .rxbuflen=sizeof(rxbuf0) },
-		{ .rxidx=0, .uart=&huart4,   .txdma=&hdma_uart4_tx,   .rxbuf=rxbuf1, .rxbuflen=sizeof(rxbuf1) }
+		{ COMMON_INIT,  .uart=&hlpuart1, .txdma=&hdma_lpuart1_tx, .rxbuf=rxbuf0, .rxbuflen=sizeof(rxbuf0), .txbuf=NULL, .txbuflen=0 },
+		{ COMMON_INIT,  .uart=&huart4,   .txdma=&hdma_uart4_tx,   .rxbuf=rxbuf1, .rxbuflen=sizeof(rxbuf1), .txbuf=NULL, .txbuflen=0 }
 };
 
 
+int serial_setup(int port, osThreadId task)
+{
+	serial_t *s = &serials[port];
+	s->taskHandle = task;
+	s->rxidx = 0;
+	serial_start_rx(port);
+	return 0;
+}
 
-void serial_start_rx(int port)
+int serial_start_rx(int port)
 {
 	serial_t *s = &serials[port];
 	//HAL_UART_Receive_DMA(&hlpuart1, rxbuf, 2*sizeof(msg_64_t));
@@ -49,9 +65,10 @@ void serial_start_rx(int port)
 	if (rc != HAL_OK) {
 		itm_debug2(DBG_SERIAL|DBG_ERR, "Ustrt", rc, s->uart->ErrorCode);
 	}
+	return rc;
 }
 
-static void serial_send_bytes(int port, const uint8_t *b, int len, int needcopy)
+ int serial_send_bytes(int port, const uint8_t *b, int len, int needcopy)
 {
 	serial_t *s = &serials[port];
 
@@ -59,28 +76,46 @@ static void serial_send_bytes(int port, const uint8_t *b, int len, int needcopy)
 		uint32_t notif = 0;
 		xTaskNotifyWait(0, 0xFFFFFFFF, &notif, portMAX_DELAY);
 	}
- 	memcpy(txbuf, b, len);
  	s->txonprogress = 1;
  	HAL_StatusTypeDef rc;
- 	if ((0)) {
- 		rc = HAL_UART_Transmit_DMA(&hlpuart1, (const uint8_t *)"hello world---", 12);
+ 	if (needcopy) {
+ 		if (len > s->txbuflen) {
+ 			itm_debug3(DBG_ERR|DBG_SERIAL, "tx big", port, len, s->txbuflen);
+ 			return -1;
+ 		}
+ 	 	memcpy(s->txbuf, b, len);
+ 		rc = HAL_UART_Transmit_DMA(s->uart, s->txbuf, len);
  	} else {
- 		rc = HAL_UART_Transmit_DMA(&hlpuart1, txbuf, len);
+ 		rc = HAL_UART_Transmit_DMA(s->uart, b, len);
  	}
 	if (rc != HAL_OK) {
 		itm_debug1(DBG_SERIAL|DBG_ERR, "TxErr", rc);
-	 	txonprogress = 0;
-		return;
+	 	s->txonprogress = 0;
+		return -1;
 	}
-
+	return 0;
 }
 
 //void HAL_UART_TxHalfCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_TxCpltCallback(_UNUSED_ UART_HandleTypeDef *huart)
 {
-	txonprogress = 0;
+	int port = -1;
+	for (int p=0; p<NUM_SERIALS; p++) {
+		if (serials[p].uart == huart) {
+			port = p;
+			break;
+		}
+	}
+	if (port < 0) {
+		itm_debug1(DBG_ERR|DBG_SERIAL, "bad uart", (int)huart);
+		return;
+	}
+
+	serial_t *s = &serials[port];
+	s->txonprogress = 0;
+
 	BaseType_t higher=0;
-	xTaskNotifyFromISR(usbTaskHandle, NOTIF_UART_TX, eSetBits, &higher);
+	xTaskNotifyFromISR(s->taskHandle, NOTIF_UART_TX, eSetBits, &higher);
 	portYIELD_FROM_ISR(higher);
 }
 
@@ -101,9 +136,21 @@ void HAL_UART_RxHalfCpltCallback(_UNUSED_ UART_HandleTypeDef *huart)
 }
 void HAL_UART_RxCpltCallback(_UNUSED_ UART_HandleTypeDef *huart)
 {
-	//itm_debug2(DBG_SERIAL, "Rx", huart->RxXferCount, huart->RxXferSize);
-	//itm_debug1(DBG_SERIAL, "RxCplt", 0);
-	// 9 bytes received
+	int port = -1;
+	for (int p=0; p<NUM_SERIALS; p++) {
+		if (serials[p].uart == huart) {
+			port = p;
+			break;
+		}
+	}
+	if (port < 0) {
+		itm_debug1(DBG_ERR|DBG_SERIAL, "bad uart", (int)huart);
+		return;
+	}
+
+	serial_t *s = &serials[port];
+	(void)s; // unused for now
+	/*
 	int offset = 0;
 	if (FRAME_M64==rxbuf[0]) {
 		// normal frame, aligned
@@ -123,6 +170,7 @@ void HAL_UART_RxCpltCallback(_UNUSED_ UART_HandleTypeDef *huart)
 	}
 	bh();
 	_start_rx(offset);
+	*/
 }
 void HAL_UARTEx_RxEventCallback(_UNUSED_ UART_HandleTypeDef *huart, _UNUSED_ uint16_t Size)
 {
@@ -131,7 +179,8 @@ void HAL_UARTEx_RxEventCallback(_UNUSED_ UART_HandleTypeDef *huart, _UNUSED_ uin
 }
 void HAL_UART_ErrorCallback(_UNUSED_ UART_HandleTypeDef *huart)
 {
-	itm_debug1(DBG_SERIAL, "RxErr", huart->ErrorCode);
+	itm_debug1(DBG_ERR|DBG_SERIAL, "RxErr", huart->ErrorCode);
+	bh();
 	if (__HAL_UART_GET_FLAG(huart, UART_FLAG_PE) != RESET) {
 		__HAL_UART_CLEAR_FLAG(huart, UART_CLEAR_PEF); /* Clear PE flag */
 	} else if (__HAL_UART_GET_FLAG(huart, UART_FLAG_FE) != RESET) {
@@ -141,6 +190,6 @@ void HAL_UART_ErrorCallback(_UNUSED_ UART_HandleTypeDef *huart)
 	} else if (__HAL_UART_GET_FLAG(huart, UART_FLAG_ORE) != RESET) {
 		__HAL_UART_CLEAR_FLAG(huart, UART_CLEAR_OREF); /* Clear ORE flag */
 	}
-	_start_rx(0);
+	//_start_rx(0);
 }
 
--- SimpleBle/serial/serial.h
+++ SimpleBle/serial/serial.h
@@ -8,8 +8,18 @@
 #ifndef SERIAL_SERIAL_H_
 #define SERIAL_SERIAL_H_
 
-void serial_start_rx(int port);
+#include "../misc.h"
+#include "main.h"
+
+int serial_setup(int port, osThreadId task);
 
 
+int serial_start_rx(int port);
+
+int serial_send_bytes(int port, const uint8_t *b, int len, int needcopy);
+
+
+#define NOTIF_UART_TX		0x00000100
+//#define NOTIF_UART_TX		0x00000100
 
 #endif /* SERIAL_SERIAL_H_ */
